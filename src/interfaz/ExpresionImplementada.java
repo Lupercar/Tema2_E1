4868229da20d29f2937a578b8d64ba9a21384c6a
luperca <hotolete@gmail.com>
2016-05-17 10:46:01 +0200

    Creo la clase base ExpresionImplementada de tipo abstracta que implementa a la interfaz IExpresion, reescribiendole el método común a las demás clases hijas de esta:
    	public String getEcuacion() {
    		// TODO Auto-generated method stub
    		return getExpresion() + " = "+ getExpresion();
    	}

---
+++ src/interfaz/ExpresionImplementada.java
@@ -0,0 +1,11 @@
+package interfaz;
+
+public abstract class ExpresionImplementada implements IExpresion {
+//	El metodo getEcuacion es comun a todos los tipos de Expresiones
+//	getExpresion = getValor; 
+	@Override
+	public String getEcuacion() {
+		// TODO Auto-generated method stub
+		return getExpresion() + " = "+ getExpresion();
+	}
+}
